4d9f047cc13c39d57eb89427ca435f0e537360e3
George Corney <haxiomic@gmail.com>
2019-03-20 19:46:40 +0000

    Type exploration

--- src/Debug.ts
+++ src/Debug.ts
@@ -18,8 +18,40 @@ export default class Debug {
         return `${node.getSourceFile().fileName}:${line}${character > 0 ? ':0' : ''}`;
     }
 
-    static logSymbol(s: ts.Symbol, depth: number) {
-        Terminal.log(this.getIndent(depth) + '<b>' + s.name + '</b>', ' <i,dim>' + this.getKeyActiveFlags(s.getFlags(), true).join(', ') + '<//>');
+    static logSymbol(checker: ts.TypeChecker, s: ts.Symbol, depth: number) {
+        
+
+        Terminal.writeFormatted(`<gray>` + Terminal.lineCaret + '<//>');
+        Terminal.writeFormatted(
+            this.getIndent(depth) +
+            '<b>' + s.name + '</b>' +
+            ' <i,dim>' + this.getActiveSymbolFlags(s.getFlags(), true).join(', ') + '<//>'
+        );
+
+        if (s.flags & ts.SymbolFlags.Property) {
+
+            let t = checker.getTypeAtLocation(s.valueDeclaration);
+
+            if (t.flags & ts.TypeFlags.AnyOrUnknown) {
+                Terminal.writeFormatted(' : <i,blue>AnyOrUnknown<//>');
+
+            } else if (t.flags & ts.TypeFlags.Intrinsic) {
+                let tIntrinsic: ts.IntrinsicType = t as ts.IntrinsicType;
+                Terminal.writeFormatted(' : <i,blue>' + tIntrinsic.intrinsicName + '<//>');
+
+            } else if ((t.flags & ts.TypeFlags.Object) && t.symbol != null) {
+
+                Terminal.writeFormatted(' : <i,blue,b>' + t.symbol.name + '<//>');
+
+            } else {
+
+                Terminal.writeFormatted(' : <i,blue>?<//>');
+            }
+            
+            Terminal.writeFormatted(' : <i,dim,blue>' + this.getActiveTypeFlags(t.flags).join(', ') + '<//>');
+        }
+
+        Terminal.write('\n')
     }
 
     static symbolPath(s: ts.Symbol): string {
@@ -73,7 +105,7 @@ export default class Debug {
     ModuleExports           = 1 << 27,  // Symbol for CommonJS `module` of `module.exports`
     ```
      */
-    static getKeyActiveFlags(value: ts.SymbolFlags, skipLessImportant: boolean) {
+    static getActiveSymbolFlags(value: ts.SymbolFlags, skipLessImportant: boolean) {
         let active = new Array<string>();
 
         if ((value & ts.SymbolFlags.None) !== 0) active.push('None');
@@ -138,6 +170,75 @@ export default class Debug {
             if ((value & ts.SymbolFlags.PropertyOrAccessor) !== 0) active.push('PropertyOrAccessor');
             if ((value & ts.SymbolFlags.ClassMember) !== 0) active.push('ClassMember');
         }
+
+        return active;
+    }
+
+    static getActiveTypeFlags(value: ts.TypeFlags) {
+        let active = new Array<string>();
+
+        if ((value & ts.TypeFlags.Any) !== 0) active.push('Any');
+        if ((value & ts.TypeFlags.Unknown) !== 0) active.push('Unknown');
+        if ((value & ts.TypeFlags.String) !== 0) active.push('String');
+        if ((value & ts.TypeFlags.Number) !== 0) active.push('Number');
+        if ((value & ts.TypeFlags.Boolean) !== 0) active.push('Boolean');
+        if ((value & ts.TypeFlags.Enum) !== 0) active.push('Enum');
+        if ((value & ts.TypeFlags.BigInt) !== 0) active.push('BigInt');
+        if ((value & ts.TypeFlags.StringLiteral) !== 0) active.push('StringLiteral');
+        if ((value & ts.TypeFlags.NumberLiteral) !== 0) active.push('NumberLiteral');
+        if ((value & ts.TypeFlags.BooleanLiteral) !== 0) active.push('BooleanLiteral');
+        if ((value & ts.TypeFlags.EnumLiteral) !== 0) active.push('EnumLiteral');
+        if ((value & ts.TypeFlags.BigIntLiteral) !== 0) active.push('BigIntLiteral');
+        if ((value & ts.TypeFlags.ESSymbol) !== 0) active.push('ESSymbol');
+        if ((value & ts.TypeFlags.UniqueESSymbol) !== 0) active.push('UniqueESSymbol');
+        if ((value & ts.TypeFlags.Void) !== 0) active.push('Void');
+        if ((value & ts.TypeFlags.Undefined) !== 0) active.push('Undefined');
+        if ((value & ts.TypeFlags.Null) !== 0) active.push('Null');
+        if ((value & ts.TypeFlags.Never) !== 0) active.push('Never');
+        if ((value & ts.TypeFlags.TypeParameter) !== 0) active.push('TypeParameter');
+        if ((value & ts.TypeFlags.Object) !== 0) active.push('Object');
+        if ((value & ts.TypeFlags.Union) !== 0) active.push('Union');
+        if ((value & ts.TypeFlags.Intersection) !== 0) active.push('Intersection');
+        if ((value & ts.TypeFlags.Index) !== 0) active.push('Index');
+        if ((value & ts.TypeFlags.IndexedAccess) !== 0) active.push('IndexedAccess');
+        if ((value & ts.TypeFlags.Conditional) !== 0) active.push('Conditional');
+        if ((value & ts.TypeFlags.Substitution) !== 0) active.push('Substitution');
+        if ((value & ts.TypeFlags.NonPrimitive) !== 0) active.push('NonPrimitive');
+        if ((value & ts.TypeFlags.AnyOrUnknown) !== 0) active.push('AnyOrUnknown');
+        if ((value & ts.TypeFlags.Nullable) !== 0) active.push('Nullable');
+        if ((value & ts.TypeFlags.Literal) !== 0) active.push('Literal');
+        if ((value & ts.TypeFlags.Unit) !== 0) active.push('Unit');
+        if ((value & ts.TypeFlags.StringOrNumberLiteral) !== 0) active.push('StringOrNumberLiteral');
+        if ((value & ts.TypeFlags.StringOrNumberLiteralOrUnique) !== 0) active.push('StringOrNumberLiteralOrUnique');
+        if ((value & ts.TypeFlags.DefinitelyFalsy) !== 0) active.push('DefinitelyFalsy');
+        if ((value & ts.TypeFlags.PossiblyFalsy) !== 0) active.push('PossiblyFalsy');
+        if ((value & ts.TypeFlags.Intrinsic) !== 0) active.push('Intrinsic');
+        if ((value & ts.TypeFlags.Primitive) !== 0) active.push('Primitive');
+        if ((value & ts.TypeFlags.StringLike) !== 0) active.push('StringLike');
+        if ((value & ts.TypeFlags.NumberLike) !== 0) active.push('NumberLike');
+        if ((value & ts.TypeFlags.BigIntLike) !== 0) active.push('BigIntLike');
+        if ((value & ts.TypeFlags.BooleanLike) !== 0) active.push('BooleanLike');
+        if ((value & ts.TypeFlags.EnumLike) !== 0) active.push('EnumLike');
+        if ((value & ts.TypeFlags.ESSymbolLike) !== 0) active.push('ESSymbolLike');
+        if ((value & ts.TypeFlags.VoidLike) !== 0) active.push('VoidLike');
+        if ((value & ts.TypeFlags.DisjointDomains) !== 0) active.push('DisjointDomains');
+        if ((value & ts.TypeFlags.UnionOrIntersection) !== 0) active.push('UnionOrIntersection');
+        if ((value & ts.TypeFlags.StructuredType) !== 0) active.push('StructuredType');
+        if ((value & ts.TypeFlags.TypeVariable) !== 0) active.push('TypeVariable');
+        if ((value & ts.TypeFlags.InstantiableNonPrimitive) !== 0) active.push('InstantiableNonPrimitive');
+        if ((value & ts.TypeFlags.InstantiablePrimitive) !== 0) active.push('InstantiablePrimitive');
+        if ((value & ts.TypeFlags.Instantiable) !== 0) active.push('Instantiable');
+        if ((value & ts.TypeFlags.StructuredOrInstantiable) !== 0) active.push('StructuredOrInstantiable');
+        if ((value & ts.TypeFlags.ObjectFlagsType) !== 0) active.push('ObjectFlagsType');
+        if ((value & ts.TypeFlags.Narrowable) !== 0) active.push('Narrowable');
+        if ((value & ts.TypeFlags.NotUnionOrUnit) !== 0) active.push('NotUnionOrUnit');
+        if ((value & ts.TypeFlags.NotPrimitiveUnion) !== 0) active.push('NotPrimitiveUnion');
+        if ((value & ts.TypeFlags.IncludesMask) !== 0) active.push('IncludesMask');
+        if ((value & ts.TypeFlags.IncludesStructuredOrInstantiable) !== 0) active.push('IncludesStructuredOrInstantiable');
+        if ((value & ts.TypeFlags.IncludesNonWideningType) !== 0) active.push('IncludesNonWideningType');
+        if ((value & ts.TypeFlags.IncludesWildcard) !== 0) active.push('IncludesWildcard');
+        if ((value & ts.TypeFlags.IncludesEmptyObject) !== 0) active.push('IncludesEmptyObject');
+        if ((value & ts.TypeFlags.GenericMappedType) !== 0) active.push('GenericMappedType');
 
         return active;
     }
