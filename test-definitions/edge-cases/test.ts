8a98725beaf3c9c15879dc4eed42a3cdba6042fa
George Corney <haxiomic@gmail.com>
2019-03-20 18:20:09 +0000

    Shhh

--- test-definitions/edge-cases/test.ts
+++ test-definitions/edge-cases/test.ts
@@ -6,7 +6,8 @@ x.field;
 let c = new EdgeCase.Cat(3);
 c.age;
 
-let d = new DefaultThing();
+// should be disallowed because of private constructor
+// let d = new DefaultThing();
 
 let b = new EdgeCase.BlobfishExported();
 b.thisClassIsExported;
