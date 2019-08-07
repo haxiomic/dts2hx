ac7197a853e96ee0245374e680c49e9a8450f439
George Corney <haxiomic@gmail.com>
2019-08-07 20:16:00 +0100

    Initial haxe source structure

---
+++ lib/typescript-externs/typescript/ts/CompilerOptions.hx
@@ -0,0 +1,3 @@
+package typescript.ts;
+
+typedef CompilerOptions = {};
---
+++ srchx/Main.hx
@@ -0,0 +1,11 @@
+class Main {
+
+    static function main() {
+        
+    }
+
+    static function dts2hx(definitionsPath: String, options: typescript.ts.CompilerOptions) {
+        
+    }
+    
+}
