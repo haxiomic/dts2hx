688cbae70d612b3d3a0f948afc7ef97826e5ad6d
George Corney <haxiomic@gmail.com>
2020-01-05 14:24:21 +0000

    Update typescript version

--- test/generated-externs/unit/types/ObjectAlias.hx
+++ test/generated-externs/unit/types/ObjectAlias.hx
@@ -5,7 +5,7 @@ package unit.types;
 Generated from: test/unit/types.d.ts:24:5
 **/
 
-typedef ObjectAlias = {	var fieldA: Float; 	var fieldB: Float; 	var fieldArrayAlias: Array<String>; 	@:optional var fieldOptional: Float; 	var computedFieldName: String; 	var sub: {	var a: Float; 	var b: Float;}; 	function methodSignature<T>(a: T): Void; 	function methodSignature(a: Float): Void; 	var methodProperty: (a: Any) -> Void; 	@:optional function methodSignatureOptional(): String; 	final readonlyField: String;};
+typedef ObjectAlias = {	var fieldA: Float; 	var fieldB: Float; 	var fieldArrayAlias: unit.types.ArrayAlias; 	@:optional var fieldOptional: Float; 	var computedFieldName: String; 	var sub: {	var a: Float; 	var b: Float;}; 	function methodSignature<T>(a: T): Void; 	function methodSignature(a: Float): Void; 	var methodProperty: (a: Any) -> Void; 	@:optional function methodSignatureOptional(): String; 	final readonlyField: String;};
 
 
 
