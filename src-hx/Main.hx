a1b9d72be4e33f520d51ad85c3fb047a4f5c42f7
George Corney <haxiomic@gmail.com>
2020-03-29 17:43:46 +0100

    Cli improvements, support module referencing (rather than paths)

--- src-hx/Main.hx
+++ src-hx/Main.hx
@@ -1,7 +1,12 @@
+import haxe.DynamicAccess;
+import typescript.Ts;
+import typescript.ts.CompilerOptions;
 import tool.HaxeTools;
 import Log.LogLevel;
 import hxargs.Args.ArgHandler;
 import js.Node;
+
+import js.Lib.debug;
 
 @:nullSafety
 class Main {
@@ -14,10 +19,13 @@ class Main {
 
         var userArgs = Node.process.argv.slice(2);
 
-        var options = {
+        var cliOptions = {
+            cwd: null,
             outputPath: 'dts2hx-output',
             tsConfigFilePath: null,
-            tsSourcePaths: new Array<String>(),
+            moduleNames: new Array<String>(),
+            moduleSearchPath: '.',
+            allDependencies: false,
             logLevel: Warning,
         }
 
@@ -27,29 +35,28 @@ class Main {
 
         var argHandler: ArgHandler;
         argHandler = hxargs.Args.generate([
-            @doc('Set output directory for generated externs (default "${options.outputPath}")')
+            @doc('Set output directory for generated externs (default "${cliOptions.outputPath}")')
             ['--output', '-o'] => (path: String) -> {
-                options.outputPath = path;
+                cliOptions.outputPath = path;
+            },
+
+            ['--moduleSearchPath', '-s'] => (moduleSearchPath: String) -> {
+                cliOptions.moduleSearchPath = moduleSearchPath;
+            },
+
+            @doc('Convert definitions of a specific module using normal typescript module resolution rules')
+            ['--module', '-m'] => (moduleName: String) -> {
+                cliOptions.moduleNames.push(moduleName);
             },
 
             @doc('Convert all dependencies referenced in package.json (that have type definitions)')
-            ['--all', '-a'] => () -> {
-                throw 'todo';
-            },
-
-            @doc('Convert definitions of a specific module in node_modules')
-            ['--module', '-m'] => (moduleName: String) -> {
-                throw 'todo';
-            },
-
-            @doc('Convert definitions at a given path')
-            ['--path', '-p'] => (path: String) -> {
-                options.tsSourcePaths.push(path);
+            ['--all'] => () -> {
+                cliOptions.allDependencies = true;
             },
 
             @doc('Set path to tsconfig file to use when processing the .d.ts files')
             '--tsconfig' => (path: String) -> {
-                options.tsConfigFilePath = path;
+                cliOptions.tsConfigFilePath = path;
             },
 
             @doc('Disable terminal colors')
@@ -63,11 +70,12 @@ class Main {
 
             @doc('Print all logs')
             '--verbose' => () -> {
-                options.logLevel = All;
+                cliOptions.logLevel = All;
             },
 
+            @doc('Module name')
             _ => (arg: String) -> {
-                throw 'conversion from module name not yet supported';
+                cliOptions.moduleNames.push(arg);
             }
         ]);
 
@@ -100,7 +108,87 @@ class Main {
             return;
         }
 
-        var dts2hx = new Dts2hx(options);
+        var log = new Log(cliOptions.logLevel != null ? cliOptions.logLevel : Warning);
+
+        var compilerOptionsOverride: Null<CompilerOptions> = try
+            if (cliOptions.tsConfigFilePath != null) {
+                var readResult = Ts.readConfigFile(cliOptions.tsConfigFilePath, (path) -> Ts.sys.readFile(path, 'utf8'));
+                if (readResult.error != null) {
+                    throw readResult.error.messageText;
+                } else {
+                    if (readResult.config != null) {
+                        readResult.config;
+                    } else {
+                        throw 'Could not read "${cliOptions}.tsConfigFilePath"';
+                    }
+                }
+            } else {
+                null;
+            }
+        catch(e: String) {
+            log.error(e);
+            Node.process.exit(1);
+            null;
+        }
+
+        var compilerOptions: CompilerOptions = compilerOptionsOverride != null ? compilerOptionsOverride : {
+            types: [], // disable automatic node_modules/@types inclusion
+        };
+
+        var host = Ts.createCompilerHost(compilerOptions);
+
+        log.log(host.getCurrentDirectory());
+
+        // add package.json dependencies to list of modules
+        if (cliOptions.allDependencies) {
+            try {
+                var packageJson = host.readFile('package.json');
+                if (packageJson == null) {
+                    var absPath = haxe.io.Path.join([host.getCurrentDirectory(), 'package.json']);
+                    throw '<b>"${absPath}"</> does not exist; could not determine dependencies';
+                }
+
+                var packageObj = haxe.Json.parse(packageJson);
+                var dependencies: DynamicAccess<String> = packageObj.dependencies != null ? packageObj.dependencies : {};
+                var devDependencies: DynamicAccess<String> = packageObj.devDependencies != null ? packageObj.devDependencies : {};
+                var allDependencies = dependencies.keys().concat(devDependencies.keys());
+
+                // check if module has typescript
+                for (moduleName in allDependencies) {
+                    var result = Ts.resolveModuleName(moduleName, cliOptions.moduleSearchPath + '/.', compilerOptions, host);
+                    if (result.resolvedModule != null) {
+                        cliOptions.moduleNames.push(moduleName);
+                    } else {
+                        log.warn('Dependency <b>"${moduleName}"</b> does not have typescript definitions');
+                    }
+                }
+            } catch (e: String) {
+                log.error(e);
+                Node.process.exit(1);
+            }
+        }
+
+        for (moduleName in cliOptions.moduleNames) {
+            var result = Ts.resolveModuleName(moduleName, cliOptions.moduleSearchPath + '/.', compilerOptions, host);
+            if (result.resolvedModule != null) {
+                Console.log('<green>${result.resolvedModule.resolvedFileName}</>', result.resolvedModule);
+                var rootPackage = if (result.resolvedModule.packageId != null) {
+                    [result.resolvedModule.packageId.name];
+                } else {
+                    var relPath: String = untyped Ts.convertToRelativePath(result.resolvedModule.resolvedFileName, host.getCurrentDirectory(), fileName -> host.getCanonicalFileName(fileName));
+                    var pathParts = relPath.split('/');
+                    pathParts.pop();
+                    pathParts;
+                }
+                var program = Ts.createProgram([result.resolvedModule.resolvedFileName], compilerOptions, host);
+            } else {
+                var failedLookupLocations: Array<String> = Reflect.field(result, 'failedLookupLocations'); // @internal field
+                log.error('Failed to find typescript for module <b>"${moduleName}"</b>. Searched the following paths:<dim>\n\t${failedLookupLocations.join('\n\t')}</>');
+                Node.process.exit(1);
+            }
+        }
+
+        // var dts2hx = new Dts2hx(options);
     }
 
     static function printDoc(argHandler: ArgHandler) {
@@ -108,12 +196,13 @@ class Main {
         Console.println('TypeScript definition to haxe extern converter');
         Console.println('');
         Console.printlnFormatted('<b>Usage:</b>');
-        Console.printlnFormatted('\tdts2hx <i,cyan>{path}</> <i,cyan>{options}</>');
+        Console.printlnFormatted('\tdts2hx <i,cyan>{moduleName}</> <i,cyan>{options}</>');
         Console.println('');
 
         Console.printlnFormatted('<b>Examples:</b>');
-        Console.printlnFormatted('\tdts2hx --path example.d.ts');
-        Console.printlnFormatted('\tdts2hx --path node_modules/@types -o .haxelib');
+        Console.printlnFormatted('\tdts2hx three');
+        Console.printlnFormatted('\tdts2hx --all --output .haxelib');
+        Console.printlnFormatted('\tdts2hx ./src/index --verbose');
         Console.println('');
 
         Console.printFormatted('<bright_white,b>Options:</>\n');
