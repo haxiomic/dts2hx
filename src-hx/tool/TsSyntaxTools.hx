e94a658a4a29242728b9313888762b59cf6b7bda
George Corney <haxiomic@gmail.com>
2020-04-04 14:09:03 +0100

    Add entityNameString()

--- src-hx/tool/TsSyntaxTools.hx
+++ src-hx/tool/TsSyntaxTools.hx
@@ -1,11 +1,28 @@
 package tool;
 
+import typescript.ts.Identifier;
+import typescript.ts.Node;
+import typescript.ts.EntityName;
+import typescript.ts.QualifiedName;
+import typescript.ts.DeclarationName;
 import typescript.ts.SyntaxKind;
+import typescript.Ts;
 
 class TsSyntaxTools {
 
 	static public function getSyntaxKindName(kind: SyntaxKind): String {
+		// Ts.tokenToString(kind);
 		return js.Syntax.code('require("typescript").SyntaxKind[{0}]', kind);
+	}
+
+	static public function entityNameString(entityName: EntityName): String {
+		// Identifier | QualifiedName
+		var node = (cast entityName: Node);
+		return switch node.kind {
+			case Identifier: (cast entityName: Identifier).escapedText;
+			case QualifiedName: entityNameString((cast entityName: QualifiedName).left) + '_' + entityNameString((cast entityName: QualifiedName).right);
+			default: throw 'EntityName has unexpected kind <b>${getSyntaxKindName(node.kind)}</>';
+		}
 	}
 
 }
