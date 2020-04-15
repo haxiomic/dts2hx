0891292e3739f66a1cbb25d516bf423e1cdc4a7d
George Corney <haxiomic@gmail.com>
2020-04-15 17:46:40 +0100

    Add run-unit.sh

---
+++ test/run-unit.sh
@@ -0,0 +1,11 @@
+../cli.js ./unit/ambient-modules --verbose --output _generated
+../cli.js ./unit/built-in --verbose --output _generated
+../cli.js ./unit/class --verbose --output _generated
+../cli.js ./unit/enum --verbose --output _generated
+../cli.js ./unit/export-modules --verbose --output _generated
+../cli.js ./unit/interface --verbose --output _generated
+../cli.js ./unit/named-tuple --verbose --output _generated
+../cli.js ./unit/symbols --verbose --output _generated
+../cli.js ./unit/tripple-slash-references --verbose --output _generated
+../cli.js ./unit/type-parameters --verbose --output _generated
+../cli.js ./unit/types --verbose --output _generated
