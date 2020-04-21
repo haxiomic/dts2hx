c393f3b5e9cf03fd122ceedb142648ff07a11e8d
George Corney <haxiomic@gmail.com>
2020-04-21 15:16:52 +0100

    Update generated test

--- test/_generated/unit/export_default/Default.hx
+++
@@ -1,4 +0,0 @@
-package unit.export_default;
-@:jsRequire("./unit/export-default", "default") extern class Default {
-	static var value : Float;
-}
---
+++ test/_generated/unit/export_default/ExampleValueModule.hx
@@ -0,0 +1,4 @@
+package unit.export_default;
+@:jsRequire("./unit/export-default", "default") extern class ExampleValueModule {
+	static var value : Float;
+}
