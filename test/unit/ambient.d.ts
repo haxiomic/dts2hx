cdee6352be88ad90b13ee556f1e3df7c34b31f9d
George Corney <haxiomic@gmail.com>
2020-04-24 11:07:57 +0100

    Add declare global to unit tests

--- test/unit/ambient.d.ts
+++ test/unit/ambient.d.ts
@@ -31,4 +31,14 @@ declare module "ambient/module" {
 		const value = 4;
 
 	}
+
+	global {
+        type ThisShouldBeGlobal2 = number;
+    }
+}
+
+declare module "ambient-with-global-inside" {
+    global {
+        type ThisShouldBeGlobal = number;
+    }
 }
--- test/unit/symbols.d.ts
+++ test/unit/symbols.d.ts
@@ -1,6 +1,9 @@
 /**
  * See https://github.com/microsoft/TypeScript/blob/master/doc/spec.md#23-declarations
  * 
+ * List of possible combinations
+ * https://www.typescriptlang.org/docs/handbook/declaration-merging.html#basic-concepts
+ * 
  * The 3 kinds of declarations possible, all using the same name 
  */
 export const ValueTypedefNamespace: string;
