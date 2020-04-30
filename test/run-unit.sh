a216a74015ce0d35d9e83961955b2e9a9aeedc92
George Corney <haxiomic@gmail.com>
2020-04-30 01:26:14 +0100

    Add noLibWrap to run-unit

--- test/run-unit.sh
+++ test/run-unit.sh
@@ -1,14 +1,14 @@
 rm -rf _generated-unit
 
-../cli.js ./unit/ambient --verbose --output _generated-unit
-../cli.js ./unit/built-in --verbose --output _generated-unit
-../cli.js ./unit/class --verbose --output _generated-unit
-../cli.js ./unit/enum --verbose --output _generated-unit
-../cli.js ./unit/export= --verbose --output _generated-unit
-../cli.js ./unit/export-default --verbose --output _generated-unit
-../cli.js ./unit/interface --verbose --output _generated-unit
-../cli.js ./unit/named-tuple --verbose --output _generated-unit
-../cli.js ./unit/symbols --verbose --output _generated-unit
-../cli.js ./unit/tripple-slash-references --verbose --output _generated-unit
-../cli.js ./unit/type-parameters --verbose --output _generated-unit
-../cli.js ./unit/types --verbose --output _generated-unit
+../cli.js ./unit/ambient --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/built-in --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/class --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/enum --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/export= --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/export-default --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/interface --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/named-tuple --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/symbols --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/tripple-slash-references --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/type-parameters --verbose --output _generated-unit --noLibWrap
+../cli.js ./unit/types --verbose --output _generated-unit --noLibWrap
