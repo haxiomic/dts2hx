e61120babf5780a42ce081f0c256c67869707802
George Corney <haxiomic@gmail.com>
2020-05-01 15:23:22 +0100

    Correct comment

--- test/unit/types.d.ts
+++ test/unit/types.d.ts
@@ -43,7 +43,10 @@ export namespace Types {
     /**
      * haxe doesn't support function-types with type parameters
      * this should translate to: 
-     * typedef FunctionTypeWithTypeParam<T> = (a: T, b: Any) -> Any;
+     * typedef FunctionTypeWithTypeParam<T> = {
+     *      @:selfCall
+     *      function call<K>(a:T, b:K):K;
+     * };
      */
     type FunctionTypeWithTypeParam<T> = <K>(a: T, b: K) => K;
 
