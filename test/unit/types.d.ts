3efd4149670035341faad587ce0ed07b48c97195
George Corney <haxiomic@gmail.com>
2020-05-01 14:01:37 +0100

    More rest type tests

--- test/unit/types.d.ts
+++ test/unit/types.d.ts
@@ -160,8 +160,9 @@ export namespace Types {
 
     // Functions
     const arrowNumberStringVoid: (a: number, noType) => void;
-    const arrowNumberTVoidTypeParam: <T>(a: number, tparam: T) => void;
+    const arrowNumberTVoidTypeParam: <T>(a: number, tParam: T, arrayTParam: Array<T>) => void;
     const arrowParamWithRest: (a: number, b: number, ...rest: Array<number>) => void;
+    const arrowParamWithRestOr: (a: number, b: number, ...rest: [] | [number]) => void;
     const arrowParamWithRestUnion: (a: number, b: number, ...rest: number[] | boolean[]) => void;
     const arrowParamWithRestTuple: (a: number, b: number, ...rest: [number]) => void;
     const arrowParamWithRestTupleUnion: (a: number, b: number, ...rest: [number] | [boolean]) => void;
