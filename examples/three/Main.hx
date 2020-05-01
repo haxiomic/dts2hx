4ca7dd9b64769fc16dc46d89cbdb646d5eaafd56
George Corney <haxiomic@gmail.com>
2020-05-01 16:32:40 +0100

    Add three.js example

---
+++ examples/three/Main.hx
@@ -0,0 +1,51 @@
+import js.Browser.*;
+import three.*;
+
+class Main {
+
+	static function main() {
+		// remove no-script message
+		var el = document.getElementById('no-script-warning');
+		el.remove();
+
+		var pixelRatio = window.devicePixelRatio;
+
+		var displayWidthPx = 800;
+		var displayHeightPx = 600;
+		
+		var canvas = document.createCanvasElement();
+		canvas.style.width = displayWidthPx + 'px';
+		canvas.style.height = displayHeightPx + 'px';
+		canvas.width = std.Math.round(displayWidthPx * pixelRatio);
+		canvas.height = std.Math.round(displayHeightPx * pixelRatio);
+		document.body.appendChild(canvas);
+
+		var scene = new three.Scene();
+		var camera = new three.PerspectiveCamera(75, canvas.width/canvas.height, 0.1, 1000);
+
+		var renderer = new three.WebGLRenderer({
+			canvas: cast canvas,
+			antialias: true,
+		});
+
+		var geometry = new three.BoxGeometry();
+		var material = new three.MeshBasicMaterial({color: 0x00FF00});
+		var cube = new three.Mesh(geometry, material);
+
+		scene.add(cube);
+
+		camera.position.z = 5;
+
+		function frameLoop(t_ms: Float) {
+			window.requestAnimationFrame(frameLoop);
+
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+
+			renderer.render(scene, camera);
+		}
+
+		frameLoop(window.performance.now());
+	}
+	
+}
---
+++ examples/three/README.md
@@ -0,0 +1,14 @@
+# Three.js Haxe Example
+
+Reproducing the [three.js introduction example](https://threejs.org/docs/#manual/en/introduction/Creating-a-scene)
+
+- Open a terminal to this directory
+- Install dependencies (three, esbuild and dts2hx) with
+
+	`npm install`
+
+	A `postinstall` script will run `dts2hx` which generates **externs/three/**
+- Build example with
+
+	`haxe build.hxml`
+- Open **index.html** to see the result!
