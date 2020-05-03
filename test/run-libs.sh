733e24d3755d594a4874092a9ee98a44e19f6cb1
George Corney <haxiomic@gmail.com>
2020-05-03 14:25:21 +0100

    Disable --verbose for lib tests

--- test/run-libs.sh
+++ test/run-libs.sh
@@ -3,6 +3,6 @@ rm -rf _generated-libs
 # Added --noLibWrap to reduce ts.* type duplication
 # By using --noLibWrap, ts.* libs can overrwrite each other, this adds a test order dependence
 # @! consider removing when types are mapped to haxe stb lib
-../cli.js node --verbose --output _generated-libs --noLibWrap
-../cli.js three --verbose --output _generated-libs --noLibWrap
-../cli.js jquery --verbose --output _generated-libs --noLibWrap
+../cli.js node --output _generated-libs --noLibWrap
+../cli.js three --output _generated-libs --noLibWrap
+../cli.js jquery --output _generated-libs --noLibWrap
