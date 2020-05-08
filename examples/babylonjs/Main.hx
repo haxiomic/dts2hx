65b4c5ff7a452bb9c6161787d89aa389a77c4a67
George Corney <haxiomic@gmail.com>
2020-05-08 15:47:54 +0100

    Add babylonjs example

---
+++ examples/babylonjs/Main.hx
@@ -0,0 +1,47 @@
+/**
+	babylonjs offers both modular and global declarations. If we use modular, `require('babylonjs')` will be emitted in the javascript, whereas if we use global, it assumes babylonjs is already available in the global scope
+
+	In this example we use the global type declarations so we don't need a bundler after haxe generates the javascript file
+
+	To use the modular version we can change the import to:
+		`import babylonjs.*;`
+**/
+import global.babylon.*;
+
+import js.Browser.*;
+
+class Main {
+	
+	static function main() {
+		final canvas = document.createCanvasElement();
+		canvas.width = 800;
+		canvas.height = 600;
+		document.body.appendChild(canvas);
+
+		var engine = new Engine(cast canvas, true, {
+			alpha: false,
+			powerPreference: 'high-performance',
+		});
+
+		var scene = new Scene(engine, {});
+		scene.clearColor.set(1, 0, 0, 1);scene.createDefaultEnvironment({
+			createGround: false
+		});
+
+		scene.createDefaultCamera(true, true, true);
+
+		var camera = (cast scene.activeCamera: ArcRotateCamera);
+		camera.radius = 2.5;
+		
+		var vertexData = VertexData.CreateBox({
+			size: 1
+		});
+
+		var cube = new Mesh('cube', scene);
+		vertexData.applyToMesh(cube, false);
+		cube.material = new PBRMaterial('cube-material', scene);
+
+		engine.runRenderLoop(() -> scene.render());
+	}
+
+}
---
+++ examples/babylonjs/README.md
@@ -0,0 +1,12 @@
+# Babylonjs Haxe Example
+
+- Open a terminal to this directory
+- Install dependencies (babylonjs and dts2hx) with
+
+	`npm install`
+
+	A [`postinstall`](https://docs.npmjs.com/misc/scripts#examples) script will run `dts2hx` which generates **externs/babylonjs/**
+- Build example with
+
+	`haxe build.hxml`
+- Open **index.html** to see the result!
