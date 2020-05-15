e11c241cf506b9914a39e7020e3dc3da1f9babaf
George Corney <haxiomic@gmail.com>
2020-05-15 17:38:25 +0100

    Remove --noLibWrap from run-libs now that stdlib mapping is active

--- test/run-libs.sh
+++ test/run-libs.sh
@@ -1,9 +1,6 @@
 rm -rf _generated-libs
 
-# Added --noLibWrap to reduce ts.* type duplication
-# By using --noLibWrap, ts.* libs can overrwrite each other, this adds a test order dependence
-# @! consider removing when types are mapped to haxe stb lib
-../cli.js node --output _generated-libs --noLibWrap
+../cli.js node --output _generated-libs
 # Convert the three + one of its submodules
-../cli.js three three/examples/jsm/controls/OrbitControls --output _generated-libs --noLibWrap
-../cli.js jquery --output _generated-libs --noLibWrap
+../cli.js three three/examples/jsm/controls/OrbitControls --output _generated-libs
+../cli.js jquery --output _generated-libs
