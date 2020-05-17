20b2979891917ea79684b3017a0bb08c46e66112
George Corney <haxiomic@gmail.com>
2020-05-17 13:44:06 +0100

    Update example script to use .haxelib

--- test/run-examples.sh
+++ test/run-examples.sh
@@ -1,35 +1,35 @@
 cd ../examples
 
 cd phaser
-rm -rf externs
+rm -rf .haxelib
 npm install --ignore-scripts
 ../../cli.js --all
 haxe build.hxml
 cd ../
 
 cd pixi.js
-rm -rf externs
+rm -rf .haxelib
 npm install --ignore-scripts
 ../../cli.js --all
 haxe build.hxml
 cd ../
 
 cd playcanvas
-rm -rf externs
+rm -rf .haxelib
 npm install --ignore-scripts
 ../../cli.js --all
 haxe build.hxml
 cd ../
 
 cd three
-rm -rf externs
+rm -rf .haxelib
 npm install --ignore-scripts
 ../../cli.js three three/examples/jsm/controls/OrbitControls
 haxe build.hxml
 cd ../
 
 cd babylonjs
-rm -rf externs
+rm -rf .haxelib
 npm install --ignore-scripts
 ../../cli.js --all
 haxe build.hxml
