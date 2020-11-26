137720051446e31b8b06e4b7b58abaf1b807d72e
George Corney <haxiomic@gmail.com>
2020-11-26 11:21:22 +0000

    Use the router

--- examples/express/Server.hx
+++ examples/express/Server.hx
@@ -2,35 +2,38 @@ class Server {
 
 	static function main() {
 		/**
-			Express Hello World Example
-			https://expressjs.com/en/starter/hello-world.html
+			Express Hello World Example with Router
+
+			For a discussion on this example see this thread:
+			https://community.haxe.org/t/how-does-one-use-routes-in-express/2701
 		**/
 		var app = Express.call();
 		var port = 3000;
-		app.get('/', (req, res, next) -> res.send('Hello World!'));
+		app.get('/', (req, res, next) -> res.send('Hello World! <a href="/birds">/birds</a>'));
 		app.listen(port, () -> trace('Example app listening at http://localhost:${port}'));
+
+		app.get('/route', (req, res, next) -> res.send('Example route'));
 
 		var router = express.Router.call_();
 
 		// middleware that is specific to this router
 		router.use(function timeLog (req, res, next) {
 			js.Browser.console.log('Time: ', Date.now());
+			(next: () -> Void)();
 			return null;
 		});
 
-		router.get('/', function (req, res, next) {
-			return null;
-		});
 		// define the home page route
-		router.get('/', function (req, res, next) {
+		router.get('/', (req, res, next) -> {
 			res.send('Birds home page');
-			return null;
 		});
+
 		// define the about route
-		router.get('/about', function (req, res, next) {
+		router.get('/about', (req, res, next) -> {
 			res.send('About birds');
-			return null;
 		});
+		
+		app.use('/birds', router.call);
 	}
 
 }
