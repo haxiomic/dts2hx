08be70b92d3e510835d6e4a2f58cdd9dcf9cbca4
George Corney <haxiomic@gmail.com>
2021-10-27 17:12:40 +0100

    Use latest build of dts2hx for all examples

--- test/run-examples.sh
+++ test/run-examples.sh
@@ -1,43 +1,25 @@
 cd ../examples
 
-cd phaser
-rm -rf .haxelib
-npm install --ignore-scripts
-../../cli.js --all
-haxe build.hxml
-cd ../
+# after installing node_modules we patch dts2hx in node_modules/.bin so that it refers to this repo's dts2hx build
+function patch_dts2hx() {
+	echo '#!/usr/bin/env node
+		console.log("Remapping dts2hx to current build");
+		require("../../../../dist/dts2hx.js");' > node_modules/.bin/dts2hx
+}
 
-cd pixi.js
-rm -rf .haxelib
-npm install --ignore-scripts
-../../cli.js --all
-haxe build.hxml
-cd ../
+function run_example() {
+	pushd "$1"
+	rm -rf .haxelib package_lock.json node_modules
+	npm install --silent --ignore-scripts
+	patch_dts2hx
+	npm run postinstall
+	haxe build.hxml || echo "TEST FALURE $1"
+	popd
+}
 
-cd playcanvas
-rm -rf .haxelib
-npm install --ignore-scripts
-../../cli.js --all
-haxe build.hxml
-cd ../
-
-cd three
-rm -rf .haxelib
-npm install --ignore-scripts
-../../cli.js three three/examples/jsm/controls/OrbitControls
-haxe build.hxml
-cd ../
-
-cd babylonjs
-rm -rf .haxelib
-npm install --ignore-scripts
-../../cli.js --all
-haxe build.hxml
-cd ../
-
-cd express
-rm -rf .haxelib
-npm install --ignore-scripts
-../../cli.js --all
-haxe build.hxml
-cd ../
+run_example phaser
+run_example pixi.js
+run_example playcanvas
+run_example three
+run_example babylonjs
+run_example express
