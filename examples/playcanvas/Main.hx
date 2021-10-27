725f625daef3473a1c44bad6e39fda321f796eaa
George Corney <haxiomic@gmail.com>
2021-10-27 16:33:15 +0100

    Use --global in playcanvas example

--- examples/playcanvas/Main.hx
+++ examples/playcanvas/Main.hx
@@ -3,6 +3,8 @@
 
 	In this example we use the global type declarations so we don't need a bundler after haxe generates the javascript file
 
+	To generate global externs we used dts2hx --global. If we wanted modular externs that import modules we can use --modular instead. By default both modular and global externs are generated
+
 	To use the modular version we can change the import to:
 		```haxe
 		import Playcanvas;
@@ -10,8 +12,8 @@
 		typedef Pc = Playcanvas;
 		```
 **/
-import global.Pc;
-import global.pc.*;
+import Pc;
+import pc.*;
 
 import js.Browser.*;
 
